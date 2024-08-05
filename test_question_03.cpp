1bf70ebc33e36f027819ab34951be0f3313f9b6b
VaishnaviM05 <vaishnavimaurya7gmail.com>
2024-08-05 12:45:45 +0530

    test question 03

---
+++ test_question_03.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    bool aadhar = true;
+    bool pan = false;
+
+    if (aadhar == true && pan == true){
+
+        cout<<"Eligible for opening an account in bank";
+
+    }
+    else{
+
+        cout<<"Not eligible";
+    }
+
+    return 0;
+
+}
