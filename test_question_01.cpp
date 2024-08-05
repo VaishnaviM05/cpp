bbc1309648818a3528facf10479cd967d600900c
VaishnaviM05 <vaishnavimaurya7gmail.com>
2024-08-05 12:22:55 +0530

    test question 01

---
+++ test_question_01.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a,b,c,d;
+
+    cout<<"Enter first number: ";
+    cin>>a;
+
+    cout<<"Enter second number: ";
+    cin>>b;
+
+    cout<<"Enter third number: ";
+    cin>>c;
+
+    cout<<"Enter forth number: ";
+    cin>>d;
+
+    cout<<"Answer is "<<(a*b)/c+b;
+
+    return 0;
+
+
+}
